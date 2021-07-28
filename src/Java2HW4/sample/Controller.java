2c7a172c0ddf3d93291558a97512a7bf26874c8d
Ryslan <biopyn@yandex.ru>
2021-07-28 19:33:19 +0300

    java2 lessons4

---
+++ src/Java2HW4/sample/Controller.java
@@ -0,0 +1,25 @@
+package Java2HW4.sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    public TextField textField;
+    @FXML
+    public TextArea textArea;
+
+    @FXML
+    public void sendHello() {
+        System.out.println("Hello");
+    }
+
+    @FXML
+    public void sendMsg(ActionEvent actionEvent) {
+        textArea.appendText(textField.getText()+"\n");
+        textField.clear();
+        textField.requestFocus();
+    }
+}
